384826f57af07508c3eee18c8855afd293b9628a
gibdike <vitoria.angel2002@gmail.com>
2020-11-21 00:07:15 -0400

    first commit

---
+++ js/animacao.js
@@ -0,0 +1,72 @@
+function CriandoBar(nome, classe) {
+    var position = document.querySelector('#bar ul');
+    var newLi = document.createElement('li');
+    var newA = document.createElement('a');
+    var link = "#" + classe;
+    var a = document.createTextNode(nome);
+    newA.appendChild(a);
+    newA.setAttribute("href", link);
+    newLi.appendChild(newA);
+    position.appendChild(newLi);
+}
+CriandoBar("1", "mymemorie")
+CriandoBar("2", "parte2")
+
+function barHover() {
+    const listt = document.querySelectorAll("#bar ul");
+    const menu = document.querySelectorAll(".bar_mark");
+    console.log(listt);
+    for (let index = 0; index < listt.length; index++) {
+        listt[index].addEventListener("mouseover", mouseOver);
+        listt[index].addEventListener("mouseout", mouseOut);
+        const text = menu[index].getAttribute('sub');
+        const newText = document.createTextNode(' -' + ' ' + text);
+
+
+        function mouseOver() {
+            listt[index].classList.add("extend");
+            listt[index].querySelector("a").appendChild(newText);
+
+        }
+
+        function mouseOut() {
+            listt[index].classList.remove("extend");
+            listt[index].querySelector("a").removeChild(newText);
+        }
+    }
+
+}
+
+function initAnimacaoScroll() {
+    const sections = document.querySelectorAll('.animation');
+    const menu = document.querySelectorAll('.s');
+    var position = document.querySelector('#menu-numeros');
+    var bar = document.querySelectorAll('.barra');
+    const cont = menu.length;
+
+    for (var i = 0; i < cont; i++) {
+        insereLi(i, position);
+    }
+
+    if (sections.length) {
+        const windowMetade = window.innerHeight * 0.5;
+
+        function animaScroll() {
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisible = (sectionTop - windowMetade) < 0;
+                if (isSectionVisible) {
+                    section.classList.add('ativo');
+
+                } else {
+                    section.classList.remove('ativo');
+
+                }
+            })
+        }
+        animaScroll();
+        window.addEventListener('scroll', animaScroll);
+    }
+}
+
+initAnimacaoScroll();
---
+++ js/controle.js
@@ -0,0 +1,83 @@
+function BoxAcitivies(titulo, cargo, descricao, caminhoimg) {
+    var classeHtml = ".activies ul";
+    var position = document.querySelector(classeHtml);
+    var newLi = document.createElement('li');
+    var newDivText = document.createElement('div');
+    newDivText.classList.add('text');
+    var newh2 = document.createElement('h2');
+    var newh3 = document.createElement('h3');
+    var newh4 = document.createElement('h4');
+    var newimg = document.createElement('img');
+    var a = document.createTextNode(titulo);
+    newh2.appendChild(a);
+    var a = document.createTextNode(cargo);
+    newh3.appendChild(a);
+    var a = document.createTextNode(descricao);
+    newh4.appendChild(a);
+    newimg.setAttribute("src", caminhoimg);
+    newDivText.appendChild(newh2);
+    newDivText.appendChild(newh3);
+    newDivText.appendChild(newh4);
+    newLi.appendChild(newimg);
+    newLi.appendChild(newDivText);
+    position.appendChild(newLi);
+    newLi.classList.add('animation');
+}
+BoxAcitivies(
+    "English Game Arena",
+    "Co-Founder and Club Leader",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elementum facilisis leo, vel fringilla est ullamcorpereget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui faucibus in ornare quam viverra",
+    "./img/WhatsApp Image 2020-11-18 at 23.44.35.jpeg",
+)
+BoxAcitivies(
+    "English Game Arena",
+    "Co-Founder and Club Leader",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elementum facilisis leo, vel fringilla est ullamcorpereget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui faucibus in ornare quam viverra",
+    "./img/WhatsApp Image 2020-11-18 at 23.44.35.jpeg",
+)
+BoxAcitivies(
+        "English Game Arena",
+        "Co-Founder and Club Leader",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elementum facilisis leo, vel fringilla est ullamcorpereget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui faucibus in ornare quam viverra",
+        "./img/WhatsApp Image 2020-11-18 at 23.44.35.jpeg",
+    )
+    //Parte dos paragrafos My mymemories e The dandelion
+function TextoComNegrito(textoGrande, textoNegrito, classeHtml) {
+    var position = document.querySelector(classeHtml);
+    var newh2 = document.createElement('h2');
+    var newNegrito = document.createElement('b');
+    var a = document.createTextNode(textoNegrito);
+    newNegrito.appendChild(a);
+    a = document.createTextNode(textoGrande);
+    newNegrito.appendChild(a);
+    newh2.appendChild(newNegrito);
+    newh2.appendChild(a);
+    position.appendChild(newh2);
+}
+//Luka aqui o primeiro elemento é o texto e o segundo é oq fica em negrito
+//Outra coisa é definir a classe então temos '.mymemories .box' ou '.thedandelion .box'
+TextoComNegrito(
+    'Her big light blue eyes were the most beautiful ones I’ve ever seen. Perhaps that’s the most vivid memory I have of her. When she looked at me, it was as if a fissure in space was opened and a light would shine in my direction—Icould feel love through her gaze. Even when she was mad, her eyes would still bring love—the purest form of it. Maybe that was when I learned what love is.',
+    "I remember her eyes. ",
+    '.mymemories .box'
+)
+TextoComNegrito(
+    'Her big light blue eyes were the most beautiful ones I’ve ever seen. Perhaps that’s the most vivid memory I have of her. When she looked at me, it was as if a fissure in space was opened and a light would shine in my direction—Icould feel love through her gaze. Even when she was mad, her eyes would still bring love—the purest form of it. Maybe that was when I learned what love is.',
+    "I remember her eyes. ",
+    '.mymemories .box'
+)
+TextoComNegrito(
+    'Her big light blue eyes were the most beautiful ones I’ve ever seen. Perhaps that’s the most vivid memory I have of her. When she looked at me, it was as if a fissure in space was opened and a light would shine in my direction—Icould feel love through her gaze. Even when she was mad, her eyes would still bring love—the purest form of it. Maybe that was when I learned what love is.',
+    "I remember her eyes. ",
+    '.mymemories .box'
+)
+TextoComNegrito(
+    'Her big light blue eyes were the most beautiful ones I’ve ever seen. Perhaps that’s the most vivid memory I have of her. When she looked at me, it was as if a fissure in space was opened and a light would shine in my direction—Icould feel love through her gaze. Even when she was mad, her eyes would still bring love—the purest form of it. Maybe that was when I learned what love is.',
+    "I remember her eyes. ",
+    '.thedandelion .box'
+)
+TextoComNegrito(
+    'Her big light blue eyes were the most beautiful ones I’ve ever seen. Perhaps that’s the most vivid memory I have of her. When she looked at me, it was as if a fissure in space was opened and a light would shine in my direction—Icould feel love through her gaze. Even when she was mad, her eyes would still bring love—the purest form of it. Maybe that was when I learned what love is.',
+    "I remember her eyes. ",
+    '.thedandelion .box'
+)
